cd8d450c369106619a447b3da2eca2dcdfdd05f1
maddy65536 <32717754+maddy65536@users.noreply.github.com>
2025-02-28 21:31:48 -0500

    the string literals were a bit silly

--- src/bitmap.rs
+++ src/bitmap.rs
@@ -14,31 +14,28 @@ pub fn make_bitmap(data: &[Vec<bool>]) -> Option<Vec<u8>> {
 
     let height = data.len();
     let width = data[0].len();
-    let pixel_length = data
-        .iter()
-        .map(|v| (v.len() * 3).next_multiple_of(4))
-        .sum::<usize>();
-    let result_length: usize = BMP_HEADER_LEN + pixel_length;
+    let pixel_length: usize = data.iter().map(|v| (v.len() * 3).next_multiple_of(4)).sum();
+    let result_length = BMP_HEADER_LEN + pixel_length;
     let mut res = vec![];
 
     // header
     res.extend_from_slice(b"BM"); // magic number
     res.extend_from_slice(&(result_length as u32).to_le_bytes()); // size
-    res.extend_from_slice(b"\x00\x00\x00\x00"); // reserved
-    res.extend_from_slice(b"\x36\x00\x00\x00"); // pixel array offset
+    res.extend_from_slice(&[0x00, 0x00, 0x00, 0x00]); // reserved
+    res.extend_from_slice(&[0x36, 0x00, 0x00, 0x00]); // pixel array offset
 
     // other header lol
-    res.extend_from_slice(b"\x28\x00\x00\x00"); // DIB header size
+    res.extend_from_slice(&[0x28, 0x00, 0x00, 0x00]); // DIB header size
     res.extend_from_slice(&(width as u32).to_le_bytes()); // width
     res.extend_from_slice(&(height as u32).to_le_bytes()); // height
-    res.extend_from_slice(b"\x01\x00"); // planes
-    res.extend_from_slice(b"\x18\x00"); // bits per pixel
-    res.extend_from_slice(b"\x00\x00\x00\x00"); // compression
+    res.extend_from_slice(&[0x01, 0x00]); // planes
+    res.extend_from_slice(&[0x18, 0x00]); // bits per pixel
+    res.extend_from_slice(&[0x00, 0x00, 0x00, 0x00]); // compression
     res.extend_from_slice(&(pixel_length as u32).to_le_bytes()); // image size
-    res.extend_from_slice(&[0xE8, 0x03, 0x00, 0x00]); // x pixels per meter
-    res.extend_from_slice(&[0xE8, 0x03, 0x00, 0x00]); // y pixels per meter
-    res.extend_from_slice(b"\x00\x00\x00\x00"); // colors in color table
-    res.extend_from_slice(b"\x00\x00\x00\x00"); // important color count
+    res.extend_from_slice(&[0x23, 0x2E, 0x00, 0x00]); // x pixels per meter
+    res.extend_from_slice(&[0x23, 0x2E, 0x00, 0x00]); // y pixels per meter
+    res.extend_from_slice(&[0x00, 0x00, 0x00, 0x00]); // colors in color table
+    res.extend_from_slice(&[0x00, 0x00, 0x00, 0x00]); // important color count
 
     // wheeeeee
     let pixel_data: Vec<u8> = data
